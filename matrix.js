da5be10c504eb593158b27ab6ec7968fbde18bcc
mmareco <90848940+mmareco@users.noreply.github.com>
2023-02-14 15:01:27 -0300

    Add files via upload

---
+++ matrix.js
@@ -0,0 +1,78 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let cw = window.innerWidth;
+let ch = window.innerHeight;
+
+
+canvas.width = cw;
+canvas.height = ch;
+
+
+window.addEventListener('resize', function(event) {
+    cw = window.innerWidth;
+    ch = window.innerHeight;
+    canvas.width = cw
+    canvas.height = ch;
+    maxColumns = cw / fontSize;
+    console.log(cw, ch)
+}, true);
+
+let charArr = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+'y', 'z', 'в','д','ё','ж','з','и','й','к','л','м','н','п','т','ф','ц',
+'ч','ш','щ','ъ','ы','ь','э','ю','я', 'ぁ','ぃ','ぅ','ぇ','ぉ','く','け','こ',
+'さ','し','す','そ','た','っ','て','と','に','ぬ','の','ひ','ふ','へ','ほ','め',
+'ょ','り','ろ','ゝ','ゞ', '1', '2', '3', '4', '5','6','7','8','9','0']
+
+let maxCharCount = 300;
+let fallingCharArr = [];
+let fontSize = 13;
+let maxColumns = cw / fontSize;
+
+
+let frames = 0;
+
+class FallingChar {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(ctx) {
+    this.value =
+      charArr[Math.floor(Math.random() * (charArr.length - 1))].toUpperCase();
+    this.speed = (Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
+
+    ctx.fillStyle = "rgba(255,0,0)";
+    ctx.font = fontSize + "px sans-serif";
+    ctx.fillText(this.value, this.x, this.y);
+    this.y += this.speed;
+
+    if (this.y > ch) {
+      this.y = (Math.random() * ch) / 2 - 50;
+      this.x = Math.floor(Math.random() * maxColumns) * fontSize;
+      this.speed = (-Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
+    }
+  }
+}
+
+let update = () => {
+  if (fallingCharArr.length < maxCharCount) {
+    let fallingChar = new FallingChar(
+      Math.floor(Math.random() * maxColumns) * fontSize,
+      (Math.random() * ch) / 2 - 50
+    );
+    fallingCharArr.push(fallingChar);
+  }
+  ctx.fillStyle = "rgba(0,0,0,0.05)";
+  ctx.fillRect(0, 0, cw, ch);
+  for (let i = 0; i < fallingCharArr.length && frames % 2 == 0; i++) {
+    fallingCharArr[i].draw(ctx);
+  }
+
+  requestAnimationFrame(update);
+  frames++;
+};
+
+update();
